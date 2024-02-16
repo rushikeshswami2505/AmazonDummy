1537cf7da0fb5757fc0fc8c4086dfe8998f8a761
Rushi <rushikesh.swami@kanakasoftware.com>
2024-02-16 18:50:38 +0530

    First time html,css and images are added

---
+++ src/main/java/com/amazon/dummy/DummyApplication.java
@@ -0,0 +1,14 @@
+package com.amazon.dummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
